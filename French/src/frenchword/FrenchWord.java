3c39984c0576e1a50dcb40e9131cbbeb8ff725fa
katie <katie@DESKTOP-M3C1DET>
2019-05-09 21:10:44 -0500

    Within French Writer add method to make a french phones file filled with french word objects. Within French Word add vowels

--- French/src/frenchword/FrenchWord.java
+++ French/src/frenchword/FrenchWord.java
@@ -2,126 +2,428 @@ package frenchword;
 import java.util.ArrayList;
 
 public class FrenchWord {
-		String word;
-		ArrayList<String> phonemes;
+	String word;
+	ArrayList<String> phonemes;
 
 
-		/* Default constructor, that takes no parameters
-		 * constructors have 3 distinguishing features
-		 *   Same name as the class
-		 *   No return type, not even void
-		 *   Are public
-		 * We use constructors to initialize the fields, 
-		 * And optionally do other setup.
-		 *
-		 * Call the constructor that has more parameters
-		 */
-		public FrenchWord(){
-			this(""); 
+	/* Default constructor, that takes no parameters
+	 * constructors have 3 distinguishing features
+	 *   Same name as the class
+	 *   No return type, not even void
+	 *   Are public
+	 * We use constructors to initialize the fields, 
+	 * And optionally do other setup.
+	 *
+	 * Call the constructor that has more parameters
+	 */
+	public FrenchWord(){
+		this(""); 
+	}
+
+
+
+	/* 
+	 * Constructor that takes a string intended to be the word field
+	 */
+	public FrenchWord(String word){
+		if(word.equals("")) {
+			this.word = word;
+			this.phonemes = new ArrayList<String>();
 		}
-
-
-
-		/* 
-		 * Constructor that takes a string intended to be the word field
-		 */
-		public FrenchWord(String word){
-			this.word = word; // Sets the class field "word" to be the parameter "word"
-			this.phonemes = this.phonemize(word);
+		else {
+		this.word = word; // Sets the class field "word" to be the parameter "word"
+		this.phonemes = this.phonemize(word);
 		}
+	}
 
 
 
-		public boolean equals(FrenchWord otherWord){
-			return this.word.equals(otherWord.word);
-		}
+	public boolean equals(FrenchWord otherWord){
+		return this.word.equals(otherWord.word);
+	}
 
 
-		/**
-		 * Creates a String representation of the Word object
-		 */
-		public String toString(){
-			return this.word + ":" + this.phonemes;
-		}
-	
-	
-		/**
-		 * 
-		 * @param w a string that is the word, to be phonemified
-		 * a whole bunch of if statements:
-		 * if string.contains(a letter assortment corresponding to a phoneme)
-		 * then add a phoneme or two to the arrayList 
-		 * loop over whole word 
-		 * @return arrayList myPho with all the phonemes in the word
-		 */
-		ArrayList<String> phonemize(String w) {
-			ArrayList<String> myPho = new ArrayList<String>();
-			String endHuh = "";
-			while (w.length() != 0) {
-				if(w.endsWith("ant" )|| 
-						w.endsWith("ont" )  &&  endHuh.equals("")){
-					myPho.add(0, "ô");
-					endHuh = String.valueOf(w.charAt(w.length()-3)); 
-					w = w.substring(0, (w.length() - 3));
-					
+	/**
+	 * Creates a String representation of the Word object
+	 */
+	public String toString(){
+		return this.word + ":" + this.phonemes;
+	}
+
+
+	/**
+	 * 
+	 * @param w a string that is the word, to be phonemified
+	 * a whole bunch of if statements:
+	 * if string.contains(a letter assortment corresponding to a phoneme)
+	 * then add a phoneme or two to the arrayList 
+	 * loop over whole word 
+	 * @return arrayList myPho with all the phonemes in the word
+	 */
+	ArrayList<String> phonemize(String w) {
+		ArrayList<String> myPho = new ArrayList<String>();
+		String endHuh = "";
+		while (w.length() != 0) {
+			if(w.endsWith("atient") && endHuh.equals("")){
+				myPho.add(0,"jâ");
+				myPho.add(0,"s");
+				myPho.add(0, "a");
+				endHuh = String.valueOf(w.charAt(w.length()-6)); 
+				w = w.substring(0, (w.length() - 6));
+				continue;
 			}
-				else if(w.endsWith("f")){
-					myPho.add(0, "f");
-					endHuh = String.valueOf(w.charAt(w.length()-1)); 
-					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("l")){
-					myPho.add(0, "l");
-					endHuh = String.valueOf(w.charAt(w.length()-1)); 
-					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("p")){
-					myPho.add(0, "p");
-					endHuh = String.valueOf(w.charAt(w.length()-1)); 
-					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("ie")){
-					myPho.add(0, "i");
-					endHuh = String.valueOf(w.charAt(w.length()-2)); 
-					w = w.substring(0, (w.length() - 2));
-					}
-				else if((w.endsWith("g") && endHuh.equals("e")) || (w.endsWith("g") && endHuh.equals("i")) ){
-					myPho.add(0, "Z");
-					endHuh = String.valueOf(w.charAt(w.length()-1)); 
-					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("o")) {
-					myPho.add(0, "o");
-					endHuh = String.valueOf(w.charAt(w.length()-1)); 
-					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("y")){
-					myPho.add(0, "i");
-					endHuh = String.valueOf(w.charAt(w.length()-1)); 
-					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("s") && (! endHuh.equals(""))){
+			else if(w.endsWith("aient") && endHuh.equals("")){
+				myPho.add(0, "é");
+				endHuh = String.valueOf(w.charAt(w.length()-5)); 
+				w = w.substring(0, (w.length() - 5));
+				continue;
+			}
+			else if(w.endsWith("èrent") && endHuh.equals("")){
+				myPho.add(0, "ér");
+				endHuh = String.valueOf(w.charAt(w.length()-5)); 
+				w = w.substring(0, (w.length() - 5));
+				continue;
+			}
+
+			else if(w.endsWith("est") && endHuh.equals("")){
+				myPho.add(0, "é");
+				endHuh = String.valueOf(w.charAt(w.length()-3)); 
+				w = w.substring(0, (w.length() - 3));
+				continue;
+			}
+			else if(w.endsWith("lle") && (! endHuh.equals(""))){
+				if(w.equals("ville")) {
+					myPho.add(0,"l");
+					myPho.add(0,"i");
+					myPho.add(0,"v");
+					endHuh = String.valueOf(w.charAt(w.length()-4)); 
+					w = w.substring(0, (w.length() - 4));
+					continue;
+				}
+				else {myPho.add(0, "j");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+				}
+			}
+			else if((w.endsWith("ant" )|| 
+					w.endsWith("ont" )|| 
+					w.endsWith("and")) &&  endHuh.equals("")){
+				myPho.add(0, "ô");
+				endHuh = String.valueOf(w.charAt(w.length()-3)); 
+				w = w.substring(0, (w.length() - 3));
+				continue;
+			}
+			else if((w.endsWith("eut") || 
+					w.endsWith("eux")) && endHuh.equals("")){
+				myPho.add(0, "ö");
+				endHuh = String.valueOf(w.charAt(w.length()-3)); 
+				w = w.substring(0, (w.length() - 3));
+				continue;
+			}
+			else if(w.endsWith("out")){
+				myPho.add(0, "u");
+				endHuh = String.valueOf(w.charAt(w.length()-3)); 
+				w = w.substring(0, (w.length() - 3));
+				continue;
+			}
+			else if(w.endsWith("tte")){
+				myPho.add(0, "t");
+				endHuh = String.valueOf(w.charAt(w.length()-3)); 
+				w = w.substring(0, (w.length() - 3));
+				continue;
+			}
+			else if(w.endsWith("ou")){
+				myPho.add(0, "u");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("eu")){
+				myPho.add(0, "ö");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("ch")){
+				myPho.add(0, "k");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("er")){
+				myPho.add(0, "a");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("ie")){
+				myPho.add(0, "i");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("gn") && (! endHuh.equals(""))){
+				myPho.add(0, "N");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("ge") || w.endsWith("gi")){
+				myPho.add(0, "Z");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if((w.endsWith("es") ||
+					w.endsWith("er") ||
+					w.endsWith("et") ||
+					w.endsWith("ed") ||
+					w.endsWith("ée") ||
+					w.endsWith("êt") ||
+					w.endsWith("ai")) && (! endHuh.equals(""))){
+				myPho.add(0, "é");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("qu") && (! endHuh.equals(""))){
+				myPho.add(0, "k");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if((w.endsWith("ss") || 
+					w.endsWith("sc"))  && (! endHuh.equals(""))){
+				myPho.add(0, "s");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("oi")){
+				myPho.add(0, "wa");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("ce") ||
+					w.endsWith("ci")){
+				myPho.add(0, "s");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("nn")){
+				myPho.add(0, "n");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("on")){
+				myPho.add(0, "ô");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("ll")){
+				myPho.add(0, "y");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("rr")){
+				myPho.add(0, "r");
+				endHuh = String.valueOf(w.charAt(w.length()-2)); 
+				w = w.substring(0, (w.length() - 2));
+				continue;
+			}
+			else if(w.endsWith("a") || 
+					w.endsWith("à") || 
+					w.endsWith("â")){
+				myPho.add(0, "a");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("à")){
+				myPho.add(0, "a");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("é")){
+				myPho.add(0, "é");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("e") && (! endHuh.equals(""))){
+				myPho.add(0, "è");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("î") || 
+					w.endsWith("i")) {
+				myPho.add(0, "i");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if((w.endsWith("o") ||
+					w.endsWith("ô") )){
+				myPho.add(0, "o");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("u") ||
+					w.endsWith("ù") ||
+					w.endsWith("ü") ||
+					w.endsWith("û")){
+				myPho.add(0, "y");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("f")){
+				myPho.add(0, "f");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("l")){
+				myPho.add(0, "l");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("p")){
+				myPho.add(0, "p");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("o")) {
+				myPho.add(0, "o");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("y")){
+				myPho.add(0, "i");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("s") && (! endHuh.equals(""))){
+				if((w.length()==1)|| (! endHuh.equals("[aeiou]"))) {
 					myPho.add(0, "s");
 					endHuh = String.valueOf(w.charAt(w.length()-1)); 
 					w = w.substring(0, (w.length() - 1));
-					}
-				else if(w.endsWith("ch")){
-					myPho.add(0, "k");
-					endHuh = String.valueOf(w.charAt(w.length()-2)); 
-					w = w.substring(0, (w.length() - 2));
-					}
-				else return myPho;
+					continue;
+				}
+				else { 
+					myPho.add(0, "z");
+					endHuh = String.valueOf(w.charAt(w.length()-1)); 
+					w = w.substring(0, (w.length() - 1));
+					continue;
+				}
+				
+			}
+			else if(w.endsWith("r")){
+				myPho.add(0, "r");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("b") && (! endHuh.equals(""))){
+				myPho.add(0, "b");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("c") && (! endHuh.equals(""))){
+				myPho.add(0, "k");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("ç") && (! endHuh.equals(""))){
+				myPho.add(0, "s");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("d") && (! endHuh.equals(""))){
+				myPho.add(0, "d");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("j") && (! endHuh.equals(""))){
+				myPho.add(0, "Z");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("k") && (! endHuh.equals(""))){
+				myPho.add(0, "k");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("m") && (! endHuh.equals(""))){
+				myPho.add(0, "m");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("n") && (! endHuh.equals(""))){
+				myPho.add(0, "n");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("t") && (! endHuh.equals(""))){
+				myPho.add(0, "t");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("v") && (! endHuh.equals(""))){
+				myPho.add(0, "v");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("w") && (! endHuh.equals(""))){
+				myPho.add(0, "v");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+			else if(w.endsWith("z") && (! endHuh.equals(""))){
+				myPho.add(0, "z");
+				endHuh = String.valueOf(w.charAt(w.length()-1)); 
+				w = w.substring(0, (w.length() - 1));
+				continue;
+			}
+
+
+
+			else return myPho;
 		}
-		
-			return myPho;///////////
-		}
-			
-			
-			
-			
-			
-		}
-		
-	
+
+		return myPho;///////////
+	}
+
+
+
+
+
+}
+
+
 
 
--- French/src/frenchword/FrenchWriter.java
+++ French/src/frenchword/FrenchWriter.java
@@ -10,95 +10,97 @@ import java.util.Collections;
 
 
 public class FrenchWriter {
-    ArrayList<FrenchWord> WordList;
-    
-    public static void main(String[] args) { 
-       // makeWordFileFromBook("src/texts/proust.txt");
-    	FrenchWord myWord = new FrenchWord("font");
-    	System.out.println(myWord);
-    	FrenchWord myWord2 = new FrenchWord("psychologie");
-    	System.out.println(myWord2);
-    
-        //makePhonemeFileFromWordList("FrenchWordList.txt");
-        //readSampleSentences();
-    }
-    // Turns a book into a word list on disk. Just word list. No phonemes
-    static void makeWordFileFromBook(String filename) {
-System.out.println("** Reading Book File: " + filename);
+	ArrayList<FrenchWord> WordList;
 
-    		try { 
-    			FileReader f = new FileReader(filename); // Declaring and constructing a FileReader object
-    			BufferedReader reader = new BufferedReader(f);
-    			// ArrayList is "generic," <> meaning it can be "an ArrayList of various types"
-    			// We have to tell the compiler which type of things it will hold
-    			ArrayList<String>frenchWordList = new ArrayList<String>();
+	public static void main(String[] args) { 
+		FrenchWriter François = new FrenchWriter();
+		// makeWordFileFromBook("src/texts/proust.txt");
+		François.makePhonemeFileFromWordList("src/texts/FrenchWordList.txt");
+		FrenchWord myWord = new FrenchWord("proust");
+		System.out.println(myWord);
+		FrenchWord myWord2 = new FrenchWord("");
+		System.out.println(myWord2);
 
-    			int count = 0;
-    			String line = reader.readLine();
-    			while (line != null) {
-    				//System.out.println("Read a line with " + line.length() + " characters.");
-    				line = line.trim();
+		//makePhonemeFileFromWordList("FrenchWordList.txt");
+		//readSampleSentences();
+	}
+	// Turns a book into a word list on disk. Just word list. No phonemes
+	static void makeWordFileFromBook(String filename) {
+		System.out.println("** Reading Book File: " + filename);
 
-    				String[] lineWords = line.split(" ");
+		try { 
+			FileReader f = new FileReader(filename); // Declaring and constructing a FileReader object
+			BufferedReader reader = new BufferedReader(f);
+			// ArrayList is "generic," <> meaning it can be "an ArrayList of various types"
+			// We have to tell the compiler which type of things it will hold
+			ArrayList<String>wordList = new ArrayList<String>();
 
-    				for(String  word : lineWords){
-    					String candidate = word.toLowerCase().replaceAll("[)[0-9](\\[\\]!,.?{} :; \\- \"\\']", "");
-    					if (candidate != null) {
-    					frenchWordList.add(candidate);  // Add the word object
-    					}
-    				}
-    				line = reader.readLine();
-    			}
-    			System.out.println("We read " + frenchWordList.size() + " words from the file");
+			int count = 0;
+			String line = reader.readLine();
+			while (line != null) {
+				//System.out.println("Read a line with " + line.length() + " characters.");
+				line = line.trim();
 
-    			//Collections.sort(wordList, (lambda (w1 w2) (w1 string<? w2)));
-    			Collections.sort(frenchWordList, (w1, w2) -> w1.compareTo(w2));
+				String[] lineWords = line.split(" ");
 
-    			// post-process the sorted list to build a new list of unique words
-    		    PrintWriter out1;
-    	        try {
-    	            out1 = new PrintWriter("src/texts/FrenchWordList.txt");  // DEBUG is the name of the file. 
-    	            // You would do something like PrintWriter("FrenchWordList.txt");
-        			String lastWord = ""; // Hold the previous word in the list
-        			for(String s : frenchWordList){
-        				if(! s.equals(lastWord)){
-        					out1.println(s);
-        					lastWord = s;
-        				}
-        			}
-    	            out1.close();
-    	        } catch (FileNotFoundException e) {
-    	            e.printStackTrace();
-    	        }
-    		
+				for(String  word : lineWords){
+					String candidate = word.toLowerCase().replaceAll("[)[0-9](\\[\\]!,.?{} :; \\- \"\\']", "");
+					if (candidate != null) {
+						wordList.add(candidate);  // Add the word object
+					}
+				}
+				line = reader.readLine();
+			}
+			System.out.println("We read " + wordList.size() + " words from the file");
 
-    			reader.close();
-    		} catch (IOException x) {
-    			System.err.format("IOException: %s\n", x);
-    		}
-    		System.out.println("Done Reading Book File"); 
-    		}
-}
-    	// Step 1, read the book, like we already do
-        //   generating the WordList as it goes.
-        
-        // Step 2, when WordList is done, save it to a file.
-        //   using the PrintWriter stuff from the email
-        
-    
-    
-    
-    // 
-    /*static void makePhonemeFileFromWordList(String filename) {
+			//Collections.sort(wordList, (lambda (w1 w2) (w1 string<? w2)));
+			Collections.sort(wordList, (w1, w2) -> w1.compareTo(w2));
+
+			// post-process the sorted list to build a new list of unique words
+			PrintWriter out1;
+			try {
+				out1 = new PrintWriter("src/texts/FrenchWordList.txt");  // DEBUG is the name of the file. 
+				// You would do something like PrintWriter("FrenchWordList.txt");
+				String lastWord = ""; // Hold the previous word in the list
+				for(String s : wordList){
+					if(! s.equals(lastWord)){
+						out1.println(s);
+						lastWord = s;
+					}
+				}
+				out1.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+
+			reader.close();
+		} catch (IOException x) {
+			System.err.format("IOException: %s\n", x);
+		}
+		System.out.println("Done Reading Book File"); 
+	}
+
+	// Step 1, read the book, like we already do
+	//   generating the WordList as it goes.
+
+	// Step 2, when WordList is done, save it to a file.
+	//   using the PrintWriter stuff from the email
+
+
+
+
+	// 
+	/*static void makePhonemeFileFromWordList(String filename) {
         // Step 1, read the word list file, like we already do
         //   generating phonemes for each as it goes, saving in
         //   ArrayList<FrenchWord> 
-        
+
         // Step 2, when WordList is done, save it to a file.
         //   using the PrintWriter stuff from the email
-        
+
     }
-    
+
     static void readSampleSentences() {
         String[] sentences = {
                 "je t'aime",    
@@ -107,4 +109,42 @@ System.out.println("** Reading Book File: " + filename);
     }
 
 }*/
-    
+	void makePhonemeFileFromWordList(String filename) {
+		System.out.println("** Reading Book File: " + filename);
+
+		try { 
+			FileReader f = new FileReader(filename); // Declaring and constructing a FileReader object
+			BufferedReader reader = new BufferedReader(f);
+			ArrayList<FrenchWord>frenchWordList = new ArrayList<FrenchWord>();
+
+			String line = reader.readLine();
+			while (line != null) {
+				//System.out.println("Read a line with " + line.length() + " characters.");
+				FrenchWord word = new FrenchWord(line);
+				frenchWordList.add(word);
+				line = reader.readLine();
+			}
+			System.out.println("We read " + frenchWordList.size() + " words from the file");
+
+			PrintWriter out1;
+			try {
+				out1 = new PrintWriter("src/texts/FrenchPhonList.txt");  // DEBUG is the name of the file. 
+				// You would do something like PrintWriter("FrenchWordList.txt");
+				for(FrenchWord s : frenchWordList){
+						out1.println(s);
+				}
+				out1.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+
+			reader.close();
+		} catch (IOException x) {
+			System.err.format("IOException: %s\n", x);
+		}
+		System.out.println("Done Reading Book File"); 
+	}
+}
+
+
